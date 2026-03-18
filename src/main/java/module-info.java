b29f2868673095d02be8f4f8fa2b9eb5011b764b
Debashish Mondal <debashishpc817@gmail.com>
2026-03-18 23:13:02 +0600

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.bashundhara_kings {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.bashundhara_kings to javafx.fxml;
+    exports com.example.bashundhara_kings;
+}
