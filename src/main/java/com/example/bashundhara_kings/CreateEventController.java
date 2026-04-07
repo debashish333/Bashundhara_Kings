779313a61c3ef9004bccfd6d08dfa5f095adb756
Debashish Mondal <debashishpc817@gmail.com>
2026-04-08 03:06:28 +0600

    update22

--- src/main/java/com/example/bashundhara_kings/CreateEventController.java
+++ src/main/java/com/example/bashundhara_kings/CreateEventController.java
@@ -4,14 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class CreateEventController implements Serializable
 {
@@ -31,13 +32,72 @@ public class CreateEventController implements Serializable
     private AnchorPane mainpane;
 
 
+    ArrayList<Eventmanager>EventmanagerList;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+
     @javafx.fxml.FXML
     public void initialize() {
+        EventmanagerList=new ArrayList<Eventmanager>();
+
+        VanueCombobox.getItems().addAll("Kings Arena (Main Stadium)", "Bashundhara Kings Training Ground","Bashundhara Sports Complex - Indoor Arena","Bashundhara Kings Youth Academy Ground","National Stadium, Dhaka (Old Paltan)","Bir Shreshtha Shaheed Shipahi Mostafa Kamal Stadium, Kamalapur");
+        typeCombobox.getItems().addAll("Home Match","Training Session","League Match (BPL)","Federation Cup Match","Jersey Launch");
 
     }
 
+    //String eventType, String description, String venue, String time, String eventID, LocalDate eventDate
     @javafx.fxml.FXML
     public void saveButtonONACTION(ActionEvent actionEvent) {
+//        Eventmanager co = new Eventmanager(
+//
+//                typeCombobox.getValue(),
+//                descriptiontextfield.getText(),
+//                VanueCombobox.getValue(),
+//                timetextfield.getText(),
+//                idtextfield.getText(),
+//                date.getValue()
+//
+//
+//
+//
+//        ) {
+//
+//        };
+        Eventmanager co = new Eventmanager(
+                typeCombobox.getValue(),
+                descriptiontextfield.getText(),
+                VanueCombobox.getValue(),
+                timetextfield.getText(),
+                idtextfield.getText(),
+                date.getValue()
+        );
+
+        File f = new File("Event.bin");
+        FileOutputStream fos;
+        ObjectOutputStream oos;
+
+        try {
+            if (f.exists()){
+                fos=new FileOutputStream(f,true);
+                oos=new AppendableObjectOutputStream(fos);
+            }
+
+            else {
+                fos=new FileOutputStream(f);
+                oos=new ObjectOutputStream(fos);
+
+            }
+
+            oos.writeObject(co);
+            oos.close();
+        }
+
+
+
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     @javafx.fxml.FXML
@@ -56,5 +116,11 @@ public class CreateEventController implements Serializable
 
     @javafx.fxml.FXML
     public void clearButtonONACTION(ActionEvent actionEvent) {
+        idtextfield.clear();
+        descriptiontextfield.clear();
+        timetextfield.clear();
+        date.getEditor().clear();
+        typeCombobox.getSelectionModel().clearSelection();
+        VanueCombobox.getSelectionModel().clearSelection();
     }
 }
--- src/main/java/com/example/bashundhara_kings/Eventmanager.java
+++ src/main/java/com/example/bashundhara_kings/Eventmanager.java
@@ -1,15 +1,14 @@
 package com.example.bashundhara_kings;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public abstract class Eventmanager extends User{
+public class Eventmanager implements Serializable {
 
-    private String eventType,description, venue,time;
-    private int eventID;
+    private String eventType,description, venue,time,eventID;
     private LocalDate eventDate;
 
-    public Eventmanager(String name, int id, int password, LocalDate dob, String eventType, String description, String venue, String time, int eventID, LocalDate eventDate) {
-        super(name, id, password, dob);
+    public Eventmanager(String eventType, String description, String venue, String time, String eventID, LocalDate eventDate) {
         this.eventType = eventType;
         this.description = description;
         this.venue = venue;
@@ -18,5 +17,63 @@ public abstract class Eventmanager extends User{
         this.eventDate = eventDate;
     }
 
+    public String getEventType() {
+        return eventType;
+    }
 
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
+    }
+
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Eventmanager{" +
+                "eventType='" + eventType + '\'' +
+                ", description='" + description + '\'' +
+                ", venue='" + venue + '\'' +
+                ", time='" + time + '\'' +
+                ", eventID='" + eventID + '\'' +
+                ", eventDate=" + eventDate +
+                '}';
+    }
 }
