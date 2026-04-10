cafb2c5a2ff54a8237b4db75c9686358e48b187d
nirupamaprova <nirupamaprova@gmail.com>
2026-04-11 03:57:16 +0600

    nirupama

---
+++ src/main/java/com/example/bashundhara_kings/nirupama/ManageRestoreData.java
@@ -0,0 +1,18 @@
+package com.example.bashundhara_kings.nirupama;
+
+public class ManageRestoreData {
+    @javafx.fxml.FXML
+    private Label s;
+    @javafx.fxml.FXML
+    private TextField ChoosebackupfileTF;
+    @javafx.fxml.FXML
+    private TextField selectbackupfileTF;
+
+    @javafx.fxml.FXML
+    public void restoredataOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BackOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/bashundhara_kings/nirupama/ViewUpdateSystem.java
@@ -0,0 +1,26 @@
+package com.example.bashundhara_kings.nirupama;
+
+public class ViewUpdateSystem {
+    @javafx.fxml.FXML
+    private Label systemsettingTF;
+
+    @javafx.fxml.FXML
+    public void savesettingsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sessiontimeoutOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void maxusersOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void SystemnameOnAction(ActionEvent actionEvent) {
+    }
+}
