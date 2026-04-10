fa22f28820b194353723bddbf3ee835e034b0f6a
Nusrat <khannidhi376@gmail.com>
2026-04-10 13:55:54 +0600

    nusrat

--- src/main/java/com/example/bashundhara_kings/nusrat/DemoController.java
+++ src/main/java/com/example/bashundhara_kings/nusrat/DemoController.java
@@ -3,5 +3,8 @@ package com.example.bashundhara_kings.nusrat;
 public class DemoController
 {
     @javafx.fxml.FXML
+    private BorderPane radiobutton;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }}
