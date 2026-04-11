07f43a562e5dc67cc8281ca962014d1fd1fc5c39
Debashish Mondal <debashishpc817@gmail.com>
2026-04-12 02:36:26 +0600

    lastUpdateformanuallViewingDashbord

--- src/main/java/com/example/bashundhara_kings/HelloApplication.java
+++ src/main/java/com/example/bashundhara_kings/HelloApplication.java
@@ -8,28 +8,26 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class  HelloApplication extends Application {
+
 //    @Override
 //    public void start(Stage stage) throws IOException {
-//<<<<<<< HEAD
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/bashundhara_kings/debashish/security_guard_dashbord.fxml"));
-//=======
-//<<<<<<< HEAD
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/bashundhara_kings/CoachDashboard.fxml"));
-//=======
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/bashundhara_kings/debashish/event_manager_dashboard.fxml"));
-//>>>>>>> 72d5d00eb621fa869391f5e1bc70a44fde1382f6
-//>>>>>>> a5af0069e80a739c877fbe51759d6b4d6a17dfc5
+//        FXMLLoader fxmlLoader = new FXMLLoader(
+//                HelloApplication.class.getResource("/com/example/bashundhara_kings/debashish/event_manager_dashboard.fxml")
+//        );
+//
 //        Scene scene = new Scene(fxmlLoader.load());
 //        stage.setTitle("BASHUNDHARA KINGS");
 //        stage.setScene(scene);
 //        stage.show();
+//    }
 //
-//
-//
-//
+//    public static void main(String[] args) {
+//        launch();
 //    }
 
+    // manually open the project.
 
+//
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
@@ -41,7 +39,9 @@ public class  HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
 }
+
